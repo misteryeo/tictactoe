f290d7267fe7051855e94d7d96b2ceae32723ea7
Andy Yeo <andy.yeo@gmail.com>
2017-05-01 15:02:55 -0700

    Create board and establish createMarker function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-  
+// Require node module readline for command line
+var rl = require('readline');
+
+// Create a 3x3 game board as nested arrays
+var gameBoard = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "]
+];
+
+// Create a method for user to place marker
+var createMarker = (position, marker) => {
+  gameBoard[position] = marker;
+}
+
